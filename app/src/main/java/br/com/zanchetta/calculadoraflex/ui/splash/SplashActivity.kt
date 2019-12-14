146d7c945c002f44e74d99566355ba21ca338863
Logon Aluno <logonaluno@local.com>
2019-12-14 12:49:20 -0300

    CalculadoraFlex - commit 2

--- app/src/main/java/br/com/zanchetta/calculadoraflex/ui/splash/SplashActivity.kt
+++ app/src/main/java/br/com/zanchetta/calculadoraflex/ui/splash/SplashActivity.kt
@@ -1,10 +1,13 @@
 package br.com.zanchetta.calculadoraflex.ui.splash
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.animation.AnimationUtils
+import androidx.core.content.ContextCompat.startActivity
 import br.com.zanchetta.calculadoraflex.R
 import br.com.zanchetta.calculadoraflex.ui.form.FormActivity
 import br.com.zanchetta.calculadoraflex.ui.login.LoginActivity
@@ -15,21 +18,38 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        carregar()
+        val preferences = getSharedPreferences(
+            "user_preferences",
+            Context.MODE_PRIVATE
+        )
+        val isFirstOpen = preferences.getBoolean("open_first", true)
+        if (isFirstOpen) {
+            markAppAlreadyOpen(preferences)
+            showSplash()
+        } else {
+            showLogin()
+        }
     }
 
-    private fun carregar() {
-//Carrega a animacao
+    private fun showSplash() {
         val anim = AnimationUtils.loadAnimation(this, R.anim.animacao_splash)
         anim.reset()
         ivLogo.clearAnimation()
-//Roda a animacao
         ivLogo.startAnimation(anim)
-//Chama a próxima tela após 3,5 segundos definido na SPLASH_DISPLAY_LENGTH
         Handler().postDelayed({
-            val proximaTela = Intent(this@SplashActivity, LoginActivity::class.java)
-            startActivity(proximaTela)
-            finish()
+            showLogin()
         }, TEMPO_AGUARDO_SPLASHSCREEN)
+    }
+
+    private fun markAppAlreadyOpen(preferences: SharedPreferences) {
+        val editor = preferences.edit()
+        editor.putBoolean("open_first", false)
+        editor.apply()
+    }
+
+    private fun showLogin() {
+        val nextScreen = Intent(this@SplashActivity, LoginActivity::class.java)
+        startActivity(nextScreen)
+        finish()
     }
 }
